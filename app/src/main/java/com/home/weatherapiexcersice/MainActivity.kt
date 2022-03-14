fb33520858074df0209977e7ba0f2f3230b09368
Jarno02 <jarno.ryhanen02@gmail.com>
2022-03-14 14:19:11 +0200

    updated gradle dependencies minor changes

--- app/src/main/java/com/home/weatherapiexcersice/MainActivity.kt
+++ app/src/main/java/com/home/weatherapiexcersice/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.home.weatherapiexcersice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+private const val TAG = "MainActivity"
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
